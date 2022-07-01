63c0dfaa4916739dfba45498a15094156a629e18
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-07-01 12:57:08 +0530

    update push

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 // import BlogCard from "./BlogsCards";
 import Particle from "../Particle";
+import calculator from "../../Assets/calculator.png";
+import movieapp from "../../Assets/movieapp.png";
+import portfolio from "../../Assets/portfolio.png";
+
 
 // import leaf from "../../Assets/Projects/leaf.png";
 // import emotion from "../../Assets/Projects/emotion.jpeg";
@@ -27,53 +31,37 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={}
-              //isBlog={false}
-              //title=""
-              //description=""
-              //link="https://github.com/beebus/"
+              imgPath={calculator}
+              isBlog={false}
+              title="Calculator"
+              description="Calculator using HTML, CSS and JavaScript."
+              link="https://shudhanshu7324.github.io/CalculatorProject/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={}
-              //isBlog={false}
-              //title=""
-              //description=""
-              //link="https://github.com/beebus/"
+              imgPath={portfolio}
+              isBlog={false}
+              title="Personal Portfolio"
+              description="Portfolio Website Which Features My Skills and Projects."
+              link="https://github.com/beebus/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={}
-              //isBlog={false}
-              //title=""
-              //description=""
-              //link="https://github.com/beebus/"
+              imgPath={movieapp}
+              isBlog={false}
+              title="Movie App"
+              description="Movie List using IMDB API and ReactJs."
+              link="https://shudhanshu7324.github.io/Movie-App/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              //imgPath={}
-              //isBlog={false}
-              //title=""
-              //description=""
-              //link="https://github.com/beebus/"
-            />
-          </Col>
+         
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              //imgPath={}
-              //isBlog={false}
-              //title=""
-              //description=""
-              //link="https://github.com/beebus/"
-            />
-          </Col>
+        
         </Row>
       </Container>
     </Container>
